d2c7ac296038b977db501168cb40b887f7b71d81
tech-camp <tech-camp@kenshuu04.local>
2018-05-07 13:45:21 +0900

    レビュー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 TechReviewSite::Application.routes.draw do
+  resources :products, only: :show do
+    resources :reviews, only: [:new, :create]
+    collection do
+      get 'search'
+    end
+  end
   root 'products#index'
-  get 'products/search' => 'products#search'
-  get 'products/:id' => 'products#show'
+
 end
